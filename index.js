90e03788555a9d1531eb038d6e218da76c15f818
suzybell <suslante1599@gmail.com>
2025-08-15 14:19:09 -0500

    Agregado endpoint raíz '/' para probar backend

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor funcionando correctamente 🚀 en el puerto ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("✅ Backend funcionando correctamente!");
+});
